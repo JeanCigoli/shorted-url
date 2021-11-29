54aa5d6a934cc2be4d2c34308819707a93be02a6
jean.almeida <jean.almeida@pagtel.com.br>
2021-11-29 17:43:53 -0300

    refactor: correct variables in .env.example

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,3 @@
-import { knexConnection } from '../infra/db/mssql/helpers';
 import { SERVER } from '../utils/config/constants';
 import errorLogger from '../utils/logger';
 import { server } from './application';
@@ -8,7 +7,6 @@ import { makeCreateMongoOnInit } from './facades';
   try {
     await makeCreateMongoOnInit().start();
 
-    // await knexConnection.raw('SELECT 1');
     server.listen(SERVER.PORT, async () => {
       console.log(`Server is running on port: ${SERVER.PORT}`);
     });
